2e9114c31cacdfd8bb86079d17578cd3c954bd2f
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-01-09 21:13:31 -0300

    Create clean function button

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -8,13 +8,17 @@ export const WeatherApp = () => {
 
   const [ciudad, setCiudad] = useState('');
   const [dataClima, setDataClima] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
   const handleCambioCiudad = (e) => {
     setCiudad(e.target.value);
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (ciudad.length > 0) fetchClima();
+    if (ciudad.length > 0) {
+      setIsVisible(true);
+      fetchClima();
+    }
   }
 
   const fetchClima = async () => {
@@ -29,24 +33,39 @@ export const WeatherApp = () => {
     }
   }
 
+  const handleClean = () => {
+    setCiudad('');
+    setDataClima(null);
+    setIsVisible(false);
+  }
+
   return (
     <div className="container">
       <h1>Aplicacion del clima</h1>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={ciudad}
-          onChange={handleCambioCiudad}
-          placeholder="Ciudad" />
-        <button type="submit">Buscar</button>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <div className="input-container">
+          <input
+            type="text"
+            value={ciudad}
+            onChange={handleCambioCiudad}
+            placeholder="Ciudad" />
+          <button type="submit">Buscar</button>
+          {
+            isVisible && (
+              <button className="clean" type="button" onClick={handleClean}>
+                Limpiar
+              </button>
+            )}
+        </div>
       </form>
       {
-        dataClima && (
+        // Si dataClima es distinto de null se muestra la informacion
+        isVisible && dataClima && (
           <div className="clima-container">
             <h2 className="clima-location">{dataClima.name}, {dataClima.sys.country}</h2>
             <p className="clima-description">{dataClima.weather[0].description}</p>
-            <img src={`http://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`} />
+            <img alt="Clima" src={`http://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`} />
             <p className="clima-temperature">Temperatura: {parseInt(dataClima?.main?.temp - difKelvin)} °C</p>
           </div>
         )
